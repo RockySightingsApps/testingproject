e4875cb501128fae0f9621a36758f7704ef3841b
Robert Smith <robertsmith@Roberts-MacBook-Air.local>
2016-10-29 22:02:46 -0400

    Initial Commit

---
+++ This Is a test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  This Is a test
+//
+//  Created by Robert Smith on 10/29/16.
+//  Copyright © 2016 TwiseApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
